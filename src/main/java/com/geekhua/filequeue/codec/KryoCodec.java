3ad616d99ce9ccb8763a044b2c719b513f3ed713
Macro Huang <macrohuang@126.com>
2012-11-21 11:30:47 +0800

    Put Input/Output into ThreadLocal, improve about 20% of mult thread use and 10% of single thread use.

--- src/main/java/com/geekhua/filequeue/codec/KryoCodec.java
+++ src/main/java/com/geekhua/filequeue/codec/KryoCodec.java
@@ -1,7 +1,5 @@
 package com.geekhua.filequeue.codec;
 
-import org.objenesis.strategy.StdInstantiatorStrategy;
-
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
@@ -14,29 +12,33 @@ import com.esotericsoftware.kryo.io.Output;
  */
 public class KryoCodec implements Codec {
 	private ThreadLocal<Kryo> serializer = new ThreadLocal<Kryo>();
+	private ThreadLocal<Output> output = new ThreadLocal<Output>();
+	private ThreadLocal<Input> input = new ThreadLocal<Input>();
 	public byte[] encode(Object element) {
 		Kryo kryo = serializer.get();
+		Output output = this.output.get();
 		if (kryo == null) {
 			kryo = new Kryo();
-			kryo.setInstantiatorStrategy(new StdInstantiatorStrategy());
 			kryo.setRegistrationRequired(false);
 			serializer.set(kryo);
+			output = new Output(1024, -1);
+			this.output.set(output);
 		}
-        Output output = new Output(1024, -1);
-        kryo.writeClassAndObject(output, element);
+		output.clear();
+		kryo.writeClassAndObject(output, element);
         return output.toBytes();
     }
 
 	public Object decode(byte[] bytes) {
 		Kryo kryo = serializer.get();
+		Input input = this.input.get();
 		if (kryo == null) {
 			kryo = new Kryo();
-			kryo.setInstantiatorStrategy(new StdInstantiatorStrategy());
 			kryo.setRegistrationRequired(false);
 			serializer.set(kryo);
+			input = new Input();
+			this.input.set(input);
 		}
-		kryo.setInstantiatorStrategy(new StdInstantiatorStrategy());
-        Input input = new Input();
         input.setBuffer(bytes);
 		return kryo.readClassAndObject(input);
     }
